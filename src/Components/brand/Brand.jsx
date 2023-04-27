79dec81b82371351351b2b1d726aa005dac2e29f
Precise Goals <elitesarthakamr@gmail.com>
2023-04-27 18:11:12 +0530

    s

--- src/Components/brand/Brand.jsx
+++ src/Components/brand/Brand.jsx
@@ -1,6 +1,11 @@
 import React from 'react' ;
 import './brand.css' ;
-import { instagram, github, replit, discord, linked } from '../../containers/header/imports';
+import instagram from '../../assests/resources/brand1.webp'
+import github from '../../assests/resources/brand2.webp'
+import replit from '../../assests/resources/brand3.webp'
+import discord from '../../assests/resources/brand4.webp'
+import linked from '../../assests/resources/brand5.webp'
+// import youtube from '../../assests/resources/brand6.webp'
 const Brand = () => {
   return (
     <div className='pg__brand section__padding'>
