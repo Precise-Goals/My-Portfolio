72693f14f7120b9399f8a9e999af45a7bde516be
Precise Goals <elitesarthakamr@gmail.com>
2023-05-12 21:13:45 +0530

    S

--- src/containers/Reviews/Reviews.jsx
+++ src/containers/Reviews/Reviews.jsx
@@ -3,7 +3,7 @@ import './reviews.css'
 
 const Reviews = () => {
   return (
-    <div id='Review' className='announcement'><h1>Uploading Reviews and Blogs Section very Soon . . . . </h1></div>
+    <div id='Review' className='reviews'><h1>Uploading Reviews and Blogs Section very Soon . . . . </h1></div>
   )
 }
 
