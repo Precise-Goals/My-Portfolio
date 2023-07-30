b654f331b53b73cedfb84216150973f2d9117e18
Precise Goals <elitesarthakamr@gmail.com>
2023-07-30 13:26:00 +0530

    some seo organising

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Work, Footer,About , Header, Reviews, } from './containers'
+import React from "react";
+import { Work, Footer, About, Header, Reviews } from "./containers";
 import { Navbar } from "./Components";
-import './App.css'
-import './fonts.css'
+import "./App.css";
+import "./fonts.css";
 const App = () => {
-  
-
   return (
-    <div className='App'>
-     <div className='gradient__bg'>
-      <Navbar />
-      <Header />
-     </div>
+    <div className="App">
+      <div className="gradient__bg">
+        {/* Starting of the Navbar */}
+        <Navbar />
+        {/* Starting of the header */}
+        <Header />
+      </div>
+      {/* My about and past history */}
       <About />
+      {/* My work fields and spaces */}
       <Work />
-      <Reviews/>
+      {/* My reviews and blogs(if any) */}
+      <Reviews />
+      {/* Footer Section */}
       <Footer />
+      {/* Made by Precise Goals  */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
