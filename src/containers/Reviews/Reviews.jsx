66d390ec8417aaddd1c24b6f9213459cabdca45d
Precise Goals <elitesarthakamr@gmail.com>
2023-07-31 22:20:13 +0530

    script evaluation

--- src/containers/Reviews/Reviews.jsx
+++ src/containers/Reviews/Reviews.jsx
@@ -3,31 +3,37 @@ import "./reviews.css";
 import { Reviewdata } from "./datas";
 
 const Reviews = () => {
+  const memoizedReviewData = React.useMemo(() => Reviewdata, []);
+
   return (
-    <div id="Review" className="reviews">
-      <div className="bgany">
-        <div className="bgimg"></div>
-        <div className="titlecontent">
-          <h1>Work Appreciations</h1>
-          <div></div>
-          <p>Here are some beloved comments regarding the work value I provided to the Clients. <br /> Thanks for giving Review :D</p>
-        </div>
-        <div className="contennt">
-          <div className="reviews">
-            {Reviewdata.map((review) => (
-              <div key={review.id} className="review-card">
-                <div className="card">
-                  <img width={60} height={60} src={review.pfp} alt={review.Username} loading="lazy" className="avatar" />
-                  <div className="username">{review.Username}</div>
-                </div>
-                <div className="message">{review.message}</div>
-              </div>
-            ))}
+    <>
+      <div id="Review" className="reviews">
+        <div className="bgany">
+          <div className="bgimg"></div>
+          <div className="titlecontent">
+            <h1>Work Appreciations</h1>
+            <div></div>
+            <p>
+              Here are some beloved comments regarding the work value I provided to the Clients. <br /> Thanks for giving Review :D
+            </p>
           </div>
+          <div className="contennt">
+            <div className="reviews">
+              {memoizedReviewData.map((review) => (
+                <div key={review.id} className="review-card">
+                  <div className="card">
+                    <img width={60} height={60} src={review.pfp} alt={review.Username} loading="lazy" className="avatar" />
+                    <div className="username">{review.Username}</div>
+                  </div>
+                  <div className="message">{review.message}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* <button><a href="#reviews">More Reviews</a></button> */}
         </div>
-        {/* <button><a href="#reviews">More Reviews</a></button> */}
       </div>
-    </div>
+    </>
   );
 };
 
--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import './about.css';
-
 import { Particle } from '../../Components';
-
-import education from '../../assets/resources/education.png'
-
+import education from '../../assets/resources/education.png';
+import handshake from '../../assets/resources/handshake.png';
 import discord from '../../assets/discord.png';
 import javascript from '../../assets/JavaScript-logo.png';
 import nodejs from '../../assets/Nodejs.png';
@@ -16,14 +14,28 @@ import canva from '../../assets/canvas.png';
 import reactnative from '../../assets/React-icon.svg.png';
 import compiler from '../../assets/c++.png';
 
-import handshake from '../../assets/resources/handshake.png'
-
 const About = () => {
   const [activeTab, setActiveTab] = useState('education');
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
+
+  // Memoize static images
+  const memoizedImages = React.useMemo(() => ({
+    education,
+    handshake,
+    javascript,
+    nodejs,
+    ps,
+    python,
+    html,
+    styles,
+    canva,
+    reactnative,
+    compiler,
+    discord,
+  }), []);
 
   return (
     <div className="aboutpage" id='About'>
@@ -66,7 +78,7 @@ const About = () => {
           {activeTab === 'education' && (
             <div className="education">
               <p>I completed my secondary education in 2022 and am now preparing for JEE while also juggling my hobbies and passions for studies and programming. I am dedicated to both my academic pursuits and my personal interests, striving to excel in all areas of my life.</p>
-              <img loading="lazy" width="250" height="167.79" src={education} alt="education" />
+              <img loading="lazy" width="250" height="167.79" src={memoizedImages.education} alt="education" />
             </div>
           )}
           {activeTab === 'skills' && (
@@ -74,18 +86,18 @@ const About = () => {
               <p>I am a skilled programmer with a keen interest in physics, and I have experience in both freelancing and managing Discord servers. Additionally, I have a talent for graphic design.</p>
               <div className="imgs" >
                 <div className="siximg">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={javascript} alt="Javascript" /></div></a>
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={html} alt="HTML 5" /></div></a>
-                  <a href="https://reactjs.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={reactnative} alt="React Js" /></div></a>
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={styles} alt="CSS" /></div></a>
-                  <a href="https://www.python.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={python} alt="Python" /></div></a>
-                  <a href="https://nodejs.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={nodejs} alt="Node Js" /></div></a>
+                  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.javascript} alt="Javascript" /></div></a>
+                  <a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.html} alt="HTML 5" /></div></a>
+                  <a href="https://reactjs.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.reactnative} alt="React Js" /></div></a>
+                  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.styles} alt="CSS" /></div></a>
+                  <a href="https://www.python.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.python} alt="Python" /></div></a>
+                  <a href="https://nodejs.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.nodejs} alt="Node Js" /></div></a>
                 </div>
                 <div className="fourimg">
-                  <a href="https://discord.js.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={discord} alt="Discord.js" /></div></a>
-                  <a href="https://www.canva.com/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={canva} alt="Canva" /></div></a>
-                  <a href="https://www.cplusplus.com/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={compiler} alt="C++" /></div></a>
-                  <a href="https://www.adobe.com/products/photoshop.html"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={ps} alt="PhotoShop" /></div></a>
+                  <a href="https://discord.js.org/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.discord} alt="Discord.js" /></div></a>
+                  <a href="https://www.canva.com/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.canva} alt="Canva" /></div></a>
+                  <a href="https://www.cplusplus.com/"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.compiler} alt="C++" /></div></a>
+                  <a href="https://www.adobe.com/products/photoshop.html"><div className="img"><img loading="lazy" width="4rem" height="4rem" src={memoizedImages.ps} alt="PhotoShop" /></div></a>
                 </div>
               </div>
             </div>
@@ -93,7 +105,7 @@ const About = () => {
           {activeTab === 'experience' && (
             <div className="exp">
               <p>I have experience in moderating Discord servers, creating websites, and designing appealing graphics. Through my skills in communication, organization, and creativity, I have successfully provided value to clients in various areas.</p>
-              <img loading="lazy" src={handshake} alt="handshake" />
+              <img loading="lazy" src={memoizedImages.handshake} alt="handshake" />
             </div>
           )}
         </div>
