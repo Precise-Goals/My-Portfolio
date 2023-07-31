728c3850074ad644fc27076e10ded1b06845a070
Precise Goals <elitesarthakamr@gmail.com>
2023-07-31 18:52:26 +0530

    Performant and interesting review changes

--- src/containers/Reviews/Reviews.jsx
+++ src/containers/Reviews/Reviews.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./reviews.css";
+import { Reviewdata } from "./datas";
 
 const Reviews = () => {
   return (
     <div id="Review" className="reviews">
-      <h1>Uploading Reviews Section very Soon . . . . </h1>
+      <div className="bgany">
+        <div className="bgimg"></div>
+        <div className="titlecontent">
+          <h1>Work Appreciations</h1>
+          <div></div>
+          <p>Here are some beloved comments regarding the work value I provided to the Clients. <br /> Thanks for giving Review :D</p>
+        </div>
+        <div className="contennt">
+          <div className="reviews">
+            {Reviewdata.map((review) => (
+              <div key={review.id} className="review-card">
+                <div className="card">
+                  <img width={60} height={60} src={review.pfp} alt={review.Username} className="avatar" />
+                  <div className="username">{review.Username}</div>
+                </div>
+                <div className="message">{review.message}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* <button><a href="#reviews">More Reviews</a></button> */}
+      </div>
     </div>
   );
 };
---
+++ src/containers/Reviews/datas.js
@@ -0,0 +1,35 @@
+import avatar1 from '../../assets/av1.png'
+import avatar2 from '../../assets/av2.png'
+import avatar3 from '../../assets/av3.png'
+import avatar4 from '../../assets/av4.png'
+
+
+
+
+export const Reviewdata =[
+    {
+        id: 1,
+        Username: "Aniket Thakur",
+        pfp:avatar1,
+        message:"Thank you for creating the incredible Discord server, 'Thakur Speaks.' Your dedication and hard work deserve 5 star rating. Your creativity, attention to detail, and commitment to a safe community are inspiring. I'm grateful for your friendship, support, and amazing talent.",
+    },
+    {
+        id: 2,
+        Username: "Pratham Handa",
+        pfp:avatar3,
+        message:"Made a very good website design according to my needs and was very happy with the result, Fully interactive Frontend which exceeded my expectations. Great Work overall.",
+    },
+    {
+        id: 3,
+        Username: "H!manshu",
+        pfp:avatar2,
+        message:"Sarthak is a progressive and determined person, gets the job done ASAP after telling him to do so. He's quite creative and impresses me with the practicality of his job.",
+    },
+    
+    {
+        id: 4,
+        Username: "Hiten aka Harsh",
+        pfp:avatar4,
+        message:"Thanks for building my server 'Homies'. I appreciated your efforts and especially efficiency as you made whole server in span of 3 days and giving me desired environment. " ,
+    }    
+]
