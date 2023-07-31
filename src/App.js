d8902cd28c8a95f89141f399b26ad6e6ad05a33b
Precise Goals <elitesarthakamr@gmail.com>
2023-07-31 10:22:23 +0530

    performance changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { Work, Footer, About, Header, Reviews } from "./containers";
+import React, { Suspense } from "react";
 import { Navbar } from "./Components";
 import "./App.css";
 import "./fonts.css";
+
+const Header = React.lazy(() => import("./containers/header/Header"));
+const About = React.lazy(() => import("./containers/about/About"));
+const Work = React.lazy(() => import("./containers/Work/Work"));
+const Reviews = React.lazy(() => import("./containers/Reviews/Reviews"));
+const Footer = React.lazy(() => import("./containers/footer/Footer") )
+
+
 const App = () => {
   return (
     <div className="App">
@@ -10,14 +17,22 @@ const App = () => {
         {/* Starting of the Navbar */}
         <Navbar />
         {/* Starting of the header */}
-        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+        </Suspense>
       </div>
       {/* My about and past history */}
-      <About />
+      <Suspense fallback={<div>Loading...</div>}>
+        <About />
+      </Suspense>
       {/* My work fields and spaces */}
-      <Work />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Work />
+      </Suspense>
       {/* My reviews and blogs(if any) */}
-      <Reviews />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Reviews />
+      </Suspense>
       {/* Footer Section */}
       <Footer />
       {/* Made by Precise Goals  */}
--- src/containers/index.js
+++
@@ -1,7 +0,0 @@
-
-export {default as Header} from './header/Header'
-export {default as About} from './about/About'
-export {default as Work} from './Work/Work'
-export {default as Footer} from './footer/Footer'
-// export {default as Feature} from './features/Feature' 
-export {default as Reviews} from './Reviews/Reviews' 
