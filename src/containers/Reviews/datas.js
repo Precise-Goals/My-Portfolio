e1e81fba14d7c52c5fddee7dac0ec595803e60ab
Precise Goals <elitesarthakamr@gmail.com>
2023-08-01 22:24:25 +0530

    shinny client review

--- src/containers/Reviews/datas.js
+++ src/containers/Reviews/datas.js
@@ -2,8 +2,7 @@ import avatar1 from '../../assets/av1.png'
 import avatar2 from '../../assets/av2.png'
 import avatar3 from '../../assets/av3.png'
 import avatar4 from '../../assets/av4.png'
-
-
+import avatar5 from '../../assets/av5.png'
 
 
 export const Reviewdata =[
@@ -11,13 +10,13 @@ export const Reviewdata =[
         id: 1,
         Username: "Aniket Thakur",
         pfp:avatar1,
-        message:"Thank you for creating the incredible Discord server, 'Thakur Speaks.' Your dedication and hard work deserve 5 star rating. Your creativity, attention to detail, and commitment to a safe community are inspiring. I'm grateful for your friendship, support, and amazing talent.",
+        message:"Thank you for creating the incredible Discord server, Your dedication and hard work deserves 5 star rating. Your creativity, attentivity to details, and commitment to a safe community are inspiring. I'm grateful for your  amicable support, and amazing talent.",
     },
     {
         id: 2,
         Username: "Pratham Handa",
         pfp:avatar3,
-        message:"Made a very good website design according to my needs and was very happy with the result, Fully interactive Frontend which exceeded my expectations. Great Work overall.",
+        message:"Made a very good website design according to my needs and I was very happy with the result. It had a fully interactive frontend which exceeded my expectations. Great Work overall.",
     },
     {
         id: 3,
@@ -25,11 +24,16 @@ export const Reviewdata =[
         pfp:avatar2,
         message:"Sarthak is a progressive and determined person, gets the job done ASAP after telling him to do so. He's quite creative and impresses me with the practicality of his job.",
     },
-    
     {
         id: 4,
         Username: "Hiten aka Harsh",
         pfp:avatar4,
         message:"Thanks for building my server 'Homies'. I appreciated your efforts and especially efficiency as you made whole server in span of 3 days and giving me desired environment. " ,
-    }    
+    },
+    {
+        id: 5,
+        Username: "just_Shinny",
+        pfp:avatar5,
+        message:" Sarthak is already next to a professional doing best coding and editing stuffs. He has created one of the best functioning servers with the best level of security putting off his hardwork. Gotta be a fan of his work, thank you for making several servers bloom within a few days",
+    },
 ]
