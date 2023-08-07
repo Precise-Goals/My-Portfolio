94f10bf2bf3fb6fa3da681a0967e95edb85fb647
Precise Goals <elitesarthakamr@gmail.com>
2023-08-07 20:44:43 +0530

    some changes

--- src/containers/Reviews/datas.js
+++ src/containers/Reviews/datas.js
@@ -14,24 +14,18 @@ export const Reviewdata =[
     },
     {
         id: 2,
-        Username: "Pratham Handa",
-        pfp:avatar3,
-        message:"Made a very good website design according to my needs and I was very happy with the result. It had a fully interactive frontend which exceeded my expectations. Great Work overall.",
-    },
-    {
-        id: 3,
         Username: "H!manshu",
         pfp:avatar2,
         message:"Sarthak is a progressive and determined person, gets the job done ASAP after telling him to do so. He's quite creative and impresses me with the practicality of his job.",
     },
     {
-        id: 4,
+        id: 3,
         Username: "Hiten aka Harsh",
         pfp:avatar4,
         message:"Thanks for building my server 'Homies'. I appreciated your efforts and especially efficiency as you made whole server in span of 3 days and giving me desired environment. " ,
     },
     {
-        id: 5,
+        id: 4,
         Username: "just_Shinny",
         pfp:avatar5,
         message:" Sarthak is already next to a professional doing best coding and editing stuffs. He has created one of the best functioning servers with the best level of security putting off his hardwork. Gotta be a fan of his work, thank you for making several servers bloom within a few days",
